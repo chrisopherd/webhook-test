187aca512dd483c70c4fa577ef228121332718ad
christopherd <christopherd@wix.com>
2020-11-17 13:20:17 -0500

    Changed port back to 80

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ router.post('/webhooks', (req, res) => {
     res.send('posting:', req.body.data)
   })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
